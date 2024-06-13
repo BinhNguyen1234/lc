58febfda734f5555a39781308941f286a29ad240
BinhNguyen1234 <binh.ngv.dev@gmail.com>
2024-06-13 18:54:10 +0200

    ok

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/cpp/googletest
-# Build directory: /root/cpp/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
