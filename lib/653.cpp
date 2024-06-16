79c3d764d65900798a6603531adf88a5393a0227
BinhNguyen1234 <binh.ngv.dev@gmail.com>
2024-06-16 10:55:45 +0200

    ok

--- lib/653.cpp
+++ lib/653.cpp
@@ -4,18 +4,9 @@
 using namespace std;
 
 bool LookupValue(TreeNode* root, int lk){
-  if(root == nullptr){
-    return false;
-  }
-  
-  if(lk == root->val) {
-    return true;
-  }
-  else if (lk < root->val){
-     return  LookupValue(root->left, lk);
-  } else {
-     return  LookupValue(root->right, lk);
-  }
+    if(root == nullptr) return false;
+    if(root->val == lk) return true;
+    return LookupValue(root->right, lk) || LookupValue(root->left, lk);
 }
 
 bool findTarget(TreeNode *root, int k){
@@ -24,34 +15,13 @@ bool findTarget(TreeNode *root, int k){
   q.push(root);
   while(!q.empty()){
     auto FirstInQ = q.front();
-
-    int val = FirstInQ->val;
-    int lookupValue = k - val;
-    int  size = q.size();
-    // how to find value
-    if(lookupValue == val ){
-      q.pop();
-    } else if (lookupValue < val){
-      q.push(FirstInQ->left);
-    } else {
-      q.push(FirstInQ->right);
-    }
-
-    //
+    int lookupValue = k - FirstInQ->val;
     if(FirstInQ->left != nullptr){
       q.push(FirstInQ->left);
     }
-
     if(FirstInQ->right != nullptr){
       q.push(FirstInQ->right);
     }
-
-
-// check all element in queue
-// 1. push element to check in queue
-// 2. get value and get remain
-// 3. if this node was be checke  before, we will ignore it to optimize the algorithm
-//
     q.pop();
   }
   return true;
