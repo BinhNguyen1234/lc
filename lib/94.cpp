bf7a97c9510c8a7510dc78132d22a5b97affb4c5
BinhNguyen1234 <binh.ngv.dev@gmail.com>
2024-07-07 11:36:07 +0200

    ok

--- lib/94.cpp
+++ lib/94.cpp
@@ -12,20 +12,22 @@ vector<int> inorderTraversal(TreeNode* root){
   TreeNode* top = stck.top();
   while(top != nullptr){
 
-    TreeNode* current = stck.top();
-    while(!stck.empty()){
-      TreeNode* left = current->left; // this casue inter loop
-      while(left != nullptr){
-	stck.push(left);
-	left = stck.top()->left;
-      }
+    TreeNode* left = top->left;
+    while(left != nullptr){
+      stck.push(left);
+      left = stck.top();
     }
-    top = stck.top();
-    stck.pop();
+    top = stck.top(); //get left
     answer.push_back(top->val);
+    stck.pop(); // remove left from stack
+    top = stck.top(); // get root
+    answer.push_back(top->val);
+
+    //get right if have
     if(top->right != nullptr){
-      stck.push(current->right);
+      stck.push(top->right);
       top = stck.top();
+      answer.push_back(top->val);
     }
   }
   return answer;
