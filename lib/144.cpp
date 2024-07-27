5f95e891062d438e902a822f6e1d508439736b8a
BinhNguyen1234 <binh.ngv.dev@gmail.com>
2024-07-27 10:16:12 +0200

    ok

--- lib/144.cpp
+++ lib/144.cpp
@@ -1,7 +1,30 @@
 #include "../include/144.h"
+#include <stack>
+using namespace std;
 
 vector<int> preorderTraversal(TreeNode *root){
   vector<int> answer;
+  stack<TreeNode*> s;
+  if(root == nullptr){
+    return answer;
+  }
+ 
+  TreeNode* current = root;
+  while(!s.empty() || current != nullptr){
+    while(current != nullptr){
+      s.push(current);
+      answer.push_back(current->val);
+      current = current->left;
+    }
+    while(!s.empty()){
+      TreeNode* top = s.top();
+      s.pop();
+      if(top->right){
+	current = top->right;
+      }
+    }
+  }
+
 
   return answer;
 }
