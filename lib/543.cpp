6ef2220245807aa25222bda34db93e1cc9aaafda
BinhNguyen1234 <binh.ngv.dev@gmail.com>
2024-08-12 10:48:39 +0200

    ok

--- lib/543.cpp
+++ lib/543.cpp
@@ -7,12 +7,22 @@ int Height(TreeNode* root){
   }
   return  1 + max(Height(root->left), Height(root->right));
 };
+int diameter(TreeNode* root, int max){
+   if(root == nullptr){
+    return max;
+  }
+  int leftHight = Height(root->left);
+  int rightHeight = Height(root->right);
+  int sumDiameter = leftHight + rightHeight;
+  if(sumDiameter > max){
+    max = sumDiameter;
+  }
+  return std::max(diameter(root->left, max), diameter(root->right, max));
+}
 int diameterOfBinaryTree(TreeNode *root){
   if(root == nullptr){
     return 0;
   }
-  int leftHight = Height(root->left);
-  int rightHeight = Height(root->right);
-  return leftHight + rightHeight;
+  return diameter(root, 0);
 }
 
