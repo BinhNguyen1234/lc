284681987446033c2e67bdb0769e6894544a67be
BinhNguyen1234 <binh.ngv.dev@gmail.com>
2024-08-14 17:35:25 +0200

    ok

--- lib/501.cpp
+++ lib/501.cpp
@@ -5,27 +5,29 @@
 #include <math.h>
 // check root->value = root->left->value || root->right> value
 // increase frequently
-int getVal(TreeNode* root, vector<int>& answer,int& currentFrequently ,int& maxFrequently){
+int inorder(TreeNode* root, vector<int>& answer,int& currentFrequently ,int& maxFrequently){
+  
+
   if(root == nullptr){
-    return NULL;
+    return -1;
   }
-  int leftValue = getVal(root->left, answer, currentFrequently, maxFrequently);
-  int rightValue = getVal(root->right, answer, currentFrequently ,maxFrequently);
-  if(leftValue == root->val){
-    currentFrequently++;
-  }
-  if(rightValue == root->val){
-    currentFrequently++;
-  }
-  if(root->val != leftValue && root->val != rightValue){
+  
+  int leftVal = inorder(root->left, answer, currentFrequently, maxFrequently);
+  int rightVal = inorder(root->right, answer, currentFrequently, maxFrequently);
+
+  if(leftVal != root->val && rightVal != root->val){
     currentFrequently = 0;
+  }
+  if(rightVal == root->val){
+    currentFrequently++;
+  }
+  if(leftVal == root->val){
+    currentFrequently++;
   }
   if(currentFrequently == maxFrequently){
     answer.push_back(root->val);
-  } else if (currentFrequently > maxFrequently){
+  } else if ( currentFrequently > maxFrequently){
     maxFrequently = currentFrequently;
-    answer.clear();
-    answer.push_back(root->val);
   }
   return root->val;
 }
@@ -33,7 +35,7 @@ vector<int> findMode(TreeNode *root){
   vector<int> answer;
   int currentFrequently = 0;
   int maxFrequently = 0;
-  getVal(root, answer, currentFrequently , maxFrequently);  
+  inorder(root, answer, currentFrequently , maxFrequently);  
 
 
 
