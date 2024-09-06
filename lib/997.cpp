98681c526bf21eaeb8b2f07b397d903bea347380
BinhNguyen1234 <binh.ngv.dev@gmail.com>
2024-09-06 05:46:01 +0200

    997

--- lib/997.cpp
+++ lib/997.cpp
@@ -1,23 +1,25 @@
 #include "../include/997.h"
 #include <vector>
-
+#include <unordered_map>
+#include <unordered_set>
+#include <iostream>
 using namespace std;
 
-void buildDsu(vector<int>& dsu, vector<vector<int>> trust){
-  for(auto& t : trust){
-    int
-
-  }
-}
-
-int findRoot(vector<int>& dsu, int vertex){
-  return dsu[vertex] == vertex ? vertext : (dsu[vertex] = findRoot(dsu, dsu[vertex]));
-}
 
 int findJudge(int n, vector<vector<int>>& trust){
-  vector<int> dsu(n);
+  unordered_map<int,unordered_set<int>> trustclub(n);
+  
   for(int i = 0; i < n; i++){
-    dsu[i] = i;
+    trustclub[i].insert(i);
+  }
+  for(auto& t: trust){
+    trustclub.erase(t[0]);
+    trustclub[t[1]].insert(t[0]);
+  }
+  for(auto& a: trustclub){
+    if(a.second.size()== n-1){
+      return a.first;
+    }
   }
   return -1;
 }
