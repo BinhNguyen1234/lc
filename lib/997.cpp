6c31f3e9e9fa8d8a97a0f1c9e73b7935f55ab772
BinhNguyen1234 <binh.ngv.dev@gmail.com>
2024-09-07 16:40:27 +0200

    997 pass edge case

--- lib/997.cpp
+++ lib/997.cpp
@@ -7,9 +7,12 @@ using namespace std;
 
 
 int findJudge(int n, vector<vector<int>>& trust){
+  //edge case
   if(trust.size() == 0){
-    return  1;
+    return  n > 1 ? -1 : 1; 
   }
+
+
   unordered_map<int,unordered_set<int>> trustclub;
   vector<bool> isTrustSomeone(n + 1, true); 
   for(auto& t: trust){
