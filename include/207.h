005b7ea289bbb3e33dba203ea3ce8f84cc927525
BinhNguyen1234 <binh.ngv.dev@gmail.com>
2024-09-16 04:46:29 +0200

    207 init

---
+++ include/207.h
@@ -0,0 +1,11 @@
+#ifndef LeetCode207
+#define LeetCode207
+
+#include <vector>
+
+using namespace std;
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites);
+
+
+
+#endif	
---
+++ lib/207.cpp
@@ -0,0 +1,6 @@
+#include "../include/207.h"
+
+
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        
+    }
