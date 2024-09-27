c8f5188af6ab334202420eeeace22e0f7ea3f927
BinhNguyen1234 <binh.ngv.dev@gmail.com>
2024-09-27 04:10:14 +0200

    210

---
+++ include/210.h
@@ -0,0 +1,12 @@
+#ifndef LeetCode210
+#define LeetCode210
+
+#include <vector>
+
+using namespace std;
+
+vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites);
+
+
+#endif
+
---
+++ lib/210.cpp
@@ -0,0 +1,7 @@
+#include "../include//210.h"
+
+vector<int> findOrder(int numCourses, vector<vector<int> > &prerequisites){
+
+}
+
+
