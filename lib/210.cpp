da0c9e220781a7595023c0fcfaf306968ba09b2a
BinhNguyen1234 <binh.ngv.dev@gmail.com>
2024-09-28 10:30:41 +0200

    210

--- lib/210.cpp
+++ lib/210.cpp
@@ -1,7 +1,42 @@
 #include "../include//210.h"
+#include <vector>
+#include <queue>
 
-vector<int> findOrder(int numCourses, vector<vector<int> > &prerequisites){
+using namespace std;
 
+vector<int> findOrder(int numCourses, vector<vector<int>> &prerequisites){
+  vector<int> answer;
+  
+  vector<vector<int>> graph(numCourses);
+  vector<int> indegree(numCourses,0);
+  
+  for(auto& p : prerequisites){
+    graph[p[0]].push_back(p[1]);
+    indegree[p[1]] += 1;
+  }
+ 
+  queue<int> q;
+  for(int  i = 0 ; i < numCourses; i++){
+    if(indegree[i] == 0){
+      q.push(i);
+    }
+  }
+  while(!q.empty()){
+    int current = q.front();
+    q.pop();
+    answer.push_back(current);
+
+    for(auto& g: graph[current]){
+      indegree[g] -= 1;
+      if(indegree[g] == 0){
+	q.push(g);
+      }
+    }
+  }
+  if(answer.size() != numCourses){
+    answer.clear();
+  }
+  return answer;
 }
 
 
