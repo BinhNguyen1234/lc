de08739bde7285a666e1ad95e48f74a0b468c897
BinhNguyen1234 <binh.ngv.dev@gmail.com>
2024-10-05 17:04:55 +0200

    399

--- lib/399.cpp
+++ lib/399.cpp
@@ -2,16 +2,20 @@
 #include <unordered_map>
 #include <vector>
 
-double DFS(unordered_map<string,unordered_map<string,double>> graph, string a, string b){
-  if(graph.find(a)== graph.end() || graph.find(b) == graph.end()){
+double DFS(unordered_map<string,unordered_map<string,double>> graph, string a, string b, unordered_map<string,bool>& visited){
+  if(graph.find(a)== graph.end() || graph.find(b) == graph.end() || (visited.find(a) != visited.end() && visited[a] == true)){
     return -1.0;
   }
+  visited[a] = true;
   if(graph.find(a) != graph.end()){
     if(graph[a].find(b) != graph[a].end()){
       return graph[a][b];
     }
+    if(a == b){
+      return 1.0;
+    }
     for(auto& bQ : graph[a]){
-      double temp = DFS(graph, bQ.first, b);
+      double temp = DFS(graph, bQ.first, b, visited);
       if(temp != -1.0){
 	return bQ.second * temp;
       }
@@ -29,7 +33,8 @@ vector<double> calcEquation(vector<vector<string>> &equations, vector<double> &v
     graph[equations[i][1]][equations[i][0]] = 1/values[i];
   }
   for(int i = 0; i < queries.size(); i++){
-    answer.push_back(DFS(graph,queries[i][0], queries[i][1]));
+    unordered_map<string, bool> visited;
+    answer.push_back(DFS(graph,queries[i][0], queries[i][1], visited));
   }
   return answer;
 }
