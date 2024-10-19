1667d7aa18df80510b11b20afa7baab3b141ec72
BinhNguyen1234 <binh.ngv.dev@gmail.com>
2024-10-19 09:05:45 +0200

    334

---
+++ include/334.h
@@ -0,0 +1,10 @@
+#ifndef LeetCode334
+#define LeetCode334
+
+#include <vector> 
+
+using namespace std;
+bool increasingTriplet(vector<int>& nums);
+
+#endif
+
---
+++ lib/334.cpp
@@ -0,0 +1,15 @@
+#include "../include/334.h"
+
+
+bool increasingTriplet(vector<int>& nums){
+  int min1 = nums[0];
+  int min2 = nums[0];
+  for(int i = 1; i < nums.size(); i++){
+    if(nums[i] <  min1){
+      min1 = nums[i];
+    }
+    if(nums[i] > min1] && nums[i] < mins)
+  }
+
+  return false;
+}
