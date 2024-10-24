a568e003f449ad3fd460dbf8794e931a128c6bb6
BinhNguyen1234 <binh.ngv.dev@gmail.com>
2024-10-24 06:58:38 +0200

    238

--- lib/238.cpp
+++ lib/238.cpp
@@ -7,7 +7,6 @@ vector<int> productExceptSelf(vector<int> &nums){
   int size = nums.size();
   for(int i = 1; i < size; i++){
     leftProduct[i] = leftProduct[i - 1] * nums[i - 1];
-    rightProduct[i] = rightProduct[size - 1 - i] * nums[i + 1];
   }
   
   for(int i = size - 2; i >= 0; i++){
